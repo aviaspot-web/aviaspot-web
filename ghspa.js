144676c35786a96c62fd6cbd6d29585b7a4f2780
aviaspot <84848664+aviaspot@users.noreply.github.com>
2021-10-25 17:21:33 +0600

    Update ghspa.js

--- ghspa.js
+++ ghspa.js
@@ -25,9 +25,12 @@
    /* redirect all 404 trafic to index.html */
    function redirect() {
      l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
+               (l.pathname ? l.pathname : ''
+               /*
               (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
               (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
               (l.hash))
+              */
    }
 
    /* resolve 404 redirects into internal routes */
