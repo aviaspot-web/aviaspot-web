01b115cda03caa8b17b3637a54f0302527106297
aviaspot <info@aviaspot.com>
2021-11-22 13:06:08 +0600

    r2021-11-22-01

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,15 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "542371c8d986b12de6fc0cc5b3b16ac1",
+  "assets/AssetManifest.json": "15c62bf724cbc838e890acb44bfc7ce6",
 "assets/assets/elastic_viewer.json": "682c80e47e133db14a4ce4cac8a7cbb2",
 "assets/assets/images/chat_wallpaper.jpg": "05a5bc82558cc49fb9f922236d7de1a0",
+"assets/assets/images/download_appstore.png": "3deb18293ea34494e1d4b67d54161d27",
+"assets/assets/images/download_googleplay.png": "78651a3d10b818bde6006ab5d28ea06e",
 "assets/assets/images/jet.png": "d18a410de4dbb45ec50b542717b2674f",
 "assets/FontManifest.json": "3ba95e877e8d977ed295b7a8efefeff7",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "a0300cf5f57546017983afb5b30decda",
+"assets/NOTICES": "b64bcf00357d23a3fd93aec5a7da267d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_AMS-Regular.ttf": "657a5353a553777e270827bd1630e467",
 "assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Caligraphic-Bold.ttf": "a9c8e437146ef63fcd6fae7cf65ca859",
@@ -56,9 +58,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "17329e37d4c3de509410105def499218",
-"/": "17329e37d4c3de509410105def499218",
-"main.dart.js": "0ed246ec8f2f37eeac04049f1639e336",
+"index.html": "1039d680b8266ce0ac4f7f391a9090c9",
+"/": "1039d680b8266ce0ac4f7f391a9090c9",
+"main.dart.js": "feca51c6907c2a2064fa651a367e4fa6",
 "manifest.json": "de13eaf27aa5f4358328215be94cd370",
 "remove_language.js": "ae220d144dbed991997753ba5220ed02",
 "version.json": "0d8c555ab88fba9325fda778479e5a9d"
